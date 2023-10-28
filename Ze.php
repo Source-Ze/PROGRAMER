3e989050297f3ae59a554b8bcd91be9ced47b1d1
Mody <143101587+Source-Ze@users.noreply.github.com>
2023-10-28 09:01:00 +0200

    Add files via upload

---
+++ Ze.php
@@ -0,0 +1,221 @@
+<script data-ad-client="ca-pub-3782750975369789" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+ </div>
+
+   </div>
+<link rel="preconnect" href="https://fonts.gstatic.com"> 
+<link href="https://fonts.googleapis.com/css2?family=Changa:wght@300&display=swap" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<html>
+<head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3782750975369789"
+     crossorigin="anonymous"></script>
+<center>
+
+<title> Ze programm</title>
+<p></p>
+    <h1>مرحبا بك عزيزي</h1>
+<p></p>
+<body background="msg.jpg">
+<br>
+</div>
+</div>
+</head>
+<body>
+<p></p>
+    <form action="" method="post">
+    <p></p>
+        <input type="text" name="search" placeholder=" رابط منشورك">
+        <p></p>
+      
+        <input type="submit" value="رشق">
+    </form>
+</body>
+</html>
+<a  href="https://t.me/amrakl" style="color:#fff;background:none;border-radius:110px;border-top:2px solid #ff0000;border-bottom:2px solid #136A8A;" class="login-link">Ze</a>
+    <marquee btn="right" >زد إي - Ze</marquee>
+   </div>      
+<style>
+.btn {
+  background-color: #E84C3D;
+  color: #fff;
+  border: none;
+  padding: 12px 24px;
+  font-size: 18px;
+  border-radius: 30px;
+  position: relative;
+  transition: all 0.3s;
+  overflow: hidden;
+  cursor: pointer;
+  z-index: 1;
+}
+
+.btn:before {
+  content: '';
+  position: absolute;
+  height: 300px;
+  width: 300px;
+  top: -100px;
+  left: -200px;
+  border-radius: 50%;
+  background-color: #ff0000;
+  transform: scale(0);
+  transition: all 0.5s;
+}
+
+.btn:hover:before {
+  transform: scale(2);
+  transition: all 0.5s;
+}
+
+.btn:after {
+  content: '';
+  position: absolute;
+  height: 100px;
+  width: 100px;
+  top: -50px;
+  right: 0;
+  border-radius: 50%;
+  background-color: #343434;
+  transform: scale(0);
+  transition: all 0.5s;
+}
+
+.btn:hover:after {
+  transform: scale(2);
+  transition: all 0.5s;
+} 
+</style> 
+<?php
+if(isset($_POST['search'])){
+    $search = $_POST['search'];
+    $data = file_get_contents("https://dev-amr1700.pantheonsite.io/Tupac/views-Telegram.php?text=".$search);
+    echo 'تم الطلب ✅';
+}
+?>
+</from>
+</center>
+<style>
+* {
+box-sizing: border-box;
+}
+
+*:focus {
+ outline: none;
+}
+body {
+font-family: 'Changa', sans-serif;
+background-size:cover;
+} 
+.login {
+margin-left:-40px;
+width: 300px;
+color:#fff;
+
+}
+.login-screen {
+background: rgba(50,50,90,.1);
+padding: 00px;
+width:340px;
+border-top:2px solid #0099BC;
+border-bottom:2px solid #0099BC;
+border-left:2px solid #21759B;
+border-right:2px solid #21759B;
+box-shadow:2px 3px 9px rgba(0,0,3,3);
+
+
+border-radius: 5px
+}
+
+.app-title {
+text-align: center;
+color: 
+}
+
+.login-form {
+text-align: center;
+}
+.control-group {
+
+}
+
+input {
+font-family: 'Changa', sans-serif;
+background-color: #ECF0F1;
+text-align:center;
+border:2px solid #ECF0F1;
+border-radius: 38px;
+font-size: 20px;
+font-weight: 800;
+padding: 8px 0;
+width: 290px;
+transition: border .5s;
+}
+input:focus {
+border-bottom: 2px solid #FF1744;
+box-shadow: none;
+}
+
+
+
+.btn {
+font-family: 'Changa', sans-serif;
+  border: 2px solid transparent;
+  background: #FF1744;
+  color: #ffffff;
+  font-size: 20px;
+  line-height: 25px;
+  padding: 10px 0;
+  text-decoration: none;
+  text-shadow: none;
+  border-radius: 3px;
+  box-shadow: none;
+  transition: 0.25s;
+  display: block;
+  width: 250px;
+  margin: 0 auto;
+}
+
+.btn:hover {
+border:1px dashed #FFFFFF;
+background:none;
+}
+.btn:foucs{
+border:1px dashed #FFFFFF;
+background:none;
+} 
+
+.login-link {
+  font-size: 20px;
+  border-radius:15px;
+  display: block;
+  background:#136A8A;
+  text-decoration:none;
+ margin-top: 12px;
+ color:#fff;
+ padding:4px;
+ 
+}
+.login-link:hover{
+border:1px dashed #fff;
+border-radius:15px;
+background:none;
+}
+.login-link:foucs{
+border:1px dashed #fff;
+border-radius:15px;
+background:none;
+} 
+img{
+width:30px;
+align-items:center;
+margin:0 auto;
+border-radius:36px;
+border-top:2px solid #136A8A;
+border-bottom:2px solid #136A8A;
+
+
+} 
+
+
